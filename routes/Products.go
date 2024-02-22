50d83add6d4dbcad448eb41c34d5b7e2b9d172c0
AhmedFawzyMof <np1265448@gmail.com>
2024-02-22 17:59:07 +0200

    H&A backend: removing cache

--- routes/Products.go
+++ routes/Products.go
@@ -1,16 +1,13 @@
 package routes
 
 import (
-	"HAstore/cache"
 	"HAstore/database"
 	"HAstore/middleware"
 	"HAstore/models"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"sync"
-	"time"
 )
 
 func AllProducts(res http.ResponseWriter, req *http.Request, params map[string]string) {
@@ -41,25 +38,6 @@ func AllProducts(res http.ResponseWriter, req *http.Request, params map[string]s
 
 	Response := make(map[string]interface{}, 1)
 	Response["Products"] = Products
-
-	cache := cache.Cache{
-		Data: Response,
-		Time: time.Now(),
-	}
-
-	if err := cache.Set("products.json"); err != nil {
-		middleware.SendError(err, res)
-		return
-	}
-
-	has, err := cache.Has("Productss", "products.json")
-
-	if err != nil {
-		middleware.SendError(err, res)
-		return
-	}
-
-	fmt.Println(has)
 
 	if err := json.NewEncoder(res).Encode(Response); err != nil {
 		middleware.SendError(err, res)
